eb49fa89e06762fc4464b8c975cdc56e5cfcd2bf
】【爪尺 Ꮆ卂爪乇 尺卂几Ꮆ乇尺 <54587dfdd@gmail.com>
2023-08-30 13:41:58 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 // import "./App.css";
+import "./index.css";
+
 
 function App() {
-
   const [movie, setMovie] = useState([]);
   const [searched, setSearched] = useState("");
+  const [movied, setMovieD] = useState("");
+
 
   const hand = async (a) => {
     a.preventDefault();
+
 
     const link = `https://www.omdbapi.com/?s=${searched}&apikey=e3b48f05`;
 
@@ -16,105 +22,140 @@ function App() {
       const data = await res.json();
       console.log(data);
       setMovie(data.Search);
+      toast.success('Here are your movies', {
+        position: "bottom-left",
+        autoClose: 200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+
+
+        
+
     } catch (error) {
       console.log(error);
+      toast.error('movie not foundwdw', {
+        position: "bottom-left",
+        autoClose: 200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        })
     }
+
+
+   
   };
 
   return (
     <>
-      <nav class="bg-black">
-        <div class="  sm:px-6 ">
-          <div class="flex h-16 items-center justify-between relative">
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <h1 className="text-white font-bold text-2xl">Movies</h1>
-            </div>
 
-            <div>
-              <form className="bg-white rounded-sm px-3 py-2 border-red-400 ">
-                <input
-                  className="outline-none"
-                  type="text"
-                  onChange={(e) => {
-                    setSearched(e.target.value);
-                  }}
-                />
-                <button onClick={hand}>
-                  <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-              </form>
-            </div>
+      <div id="main">
+        <ToastContainer />
+        <nav class="bg-black fixed w-full ">
+          <div class="  sm:px-6 ">
+            <div class="flex h-16 items-center justify-between relative ">
+              <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <h1 className="text-white font-bold text-2xl">Movies</h1>
+              </div>
 
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <div class="relative ml-3">
-                <div>
-                  <button
-                    type="button"
-                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    id="user-menu-button"
-                    aria-expanded="false"
-                    aria-haspopup="true"
-                  >
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">Open user menu</span>
-                    <img
-                      class="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
+              <div>
+                <form className="bg-white rounded-sm px-3 py-2 border-red-400 ">
+                  <input
+                    className="outline-none"
+                    type="text"
+                    onChange={(e) => {
+                      setSearched(e.target.value);
+                    }}
+                  />
+                  <button onClick={hand}>
+                    <i class="fa-solid fa-magnifying-glass" onClick={() => {
+                    
+                    }}></i>
                   </button>
+                </form>
+              </div>
+
+              <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div class="relative ml-3">
+                  <div>
+                    <button
+                      type="button"
+                      class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      id="user-menu-button"
+                      aria-expanded="false"
+                      aria-haspopup="true"
+                    >
+                      <span class="absolute -inset-1.5"></span>
+                      <span class="sr-only">Open user menu</span>
+                      <img
+                        class="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-    
+        <div>{searched.Poster}</div>
 
-      <div className="">
-        {movie?.map((pack) => {
-          const { Title, Poster, Year, Type } = pack;
-          return (
-            <>
-              <div className="container w-90 mx-auto">
-                <div className="flex py-10 border-gray-900">
-                  <img
-                    className="rounded-lg border-white border"
-                    src={Poster}
-                    alt=""
-                  />
-                  <div className="py-20 px-5">
-                    <a href="">
-                      <h1 className=" font-bold text-3xl text-white">
-                        {" "}
-                        {Title}
-                      </h1>
-                    </a>
-                    <h2 className="text-2xl py-2 font-medium text-red-400">
-                      Releas : {Year}
-                    </h2>
-                    <h2 className="text-white uppercase">{Type}</h2>
-                    <h3 className="text-1xl font-semibold text-white pt-2">
-                      Description
-                    </h3>
-                    <p className="text-white">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Reiciendis fugit illum sequi quo suscipit nulla, delectus
-                      repudiandae qui reprehenderit quae asperiores eligendi
-                      quaerat laborum recusandae dolores dolore quibusdam neque
-                      ipsum.
-                    </p>
-                    <h3 className="pt-3 text-white">
-                      Rating |{" "}
-                      <span className="font-bold text-red-500">12+</span>
-                    </h3>
+        <div className="pt-16">
+          {movie?.map((pack) => {
+            const { Title, Poster, Year, Type } = pack;
+            return (
+              <>
+                <div className="">
+                  <div className="container w-90 mx-auto">
+                    <div className="flex py-10 border-gray-900">
+                      <img
+                        className="rounded-lg border-white border bg-fixed"
+                        src={Poster}
+                        alt=""
+                      />
+                      <div className="py-20 px-5">
+                        <a href="">
+                          <h1 className=" font-bold text-3xl text-white">
+                            {" "}
+                            {Title}
+                          </h1>
+                        </a>
+                        <h2 className="text-2xl py-2 font-medium text-red-400">
+                          Releas : {Year}
+                        </h2>
+                        <h2 className="text-white uppercase">{Type}</h2>
+                        <h3 className="text-1xl font-semibold text-white pt-2">
+                          Description
+                        </h3>
+                        <p className="text-white">
+                          Lorem ipsum dolor, sit amet consectetur adipisicing
+                          elit. Reiciendis fugit illum sequi quo suscipit nulla,
+                          delectus repudiandae qui reprehenderit quae asperiores
+                          eligendi quaerat laborum recusandae dolores dolore
+                          quibusdam neque ipsum.
+                        </p>
+                        <h3 className="pt-3 text-white">
+                          Rating |{" "}
+                          <span className="font-bold text-red-500">12+</span>
+                        </h3>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
